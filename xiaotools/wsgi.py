9fef1947193c1dcb0469fea0bf705fb01e5dee16
InitialHeart0304 <143080090+InitialHeart0304@users.noreply.github.com>
2026-07-07 12:35:59 +0800

    Update wsgi.py

--- xiaotools/wsgi.py
+++ xiaotools/wsgi.py
@@ -7,4 +7,4 @@ from web.app import app
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, threaded=True)
